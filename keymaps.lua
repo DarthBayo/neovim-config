12f56fd591e74c13753efd93bf323202095cbd2d
Giovani <giovani@pop-os.localdomain>
2024-04-06 12:14:21 -0300

    qwerty

---
+++ keymaps.lua
@@ -0,0 +1,18 @@
+
+vim.g.mapleader = '\\'
+
+-----------------------------------------------------------
+-- Normal keys
+-----------------------------------------------------------
+vim.keymap.set('n', '<leader>w', ':w<cr>')
+vim.keymap.set('n', '<leader>q', ':q<cr>')
+vim.keymap.set('n', '<Esc>', ':noh<cr>')
+
+-----------------------------------------------------------
+-- EXPLORE
+-----------------------------------------------------------
+vim.keymap.set('n', '<leader>ee', vim.cmd.Ex)
+vim.keymap.set('n', '<leader>ev', vim.cmd.Ve)
+vim.keymap.set('n', '<leader>eh', vim.cmd.He)
+vim.keymap.set('n', '<leader>er', vim.cmd.Re)
+
